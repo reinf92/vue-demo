ea21a40bace83148836e6c849c1daec537411e21
reinf92 <reinf92@naver.com>
2021-03-14 22:55:30 +0900

    입문예제

---
+++ chapter07/vue-cli/src/components/TodoHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <h1>TODO List!</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+    body {
+        text-align: center;
+        background-color: #f6f6f8;
+    }
+
+    h1 {
+        color: #1f2b41;
+        font-weight: 900;
+        margin: 2.5rem 0 1 1.5rem;
+    }
+</style>
